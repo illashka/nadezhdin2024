eb9b0ade06a0f4b4fab6c0f9972c98bc5432dbd4
illashka <93211852+illashka@users.noreply.github.com>
2024-01-28 22:33:52 +0300

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,9 @@
+let i = 0;
+let votes = document.getElementsByClassName('progressbar__el__text');
+let sum = 0;
+
+for (i of votes){
+    if (i.textContent.slice(18) != 'ба'){
+        sum = sum + parseInt(i.textContent.slice(18))/2}}
+
+console.log('Количество подписей у Надеждина: ' + sum);
