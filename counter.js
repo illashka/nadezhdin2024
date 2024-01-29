ea6d3e63b9c03ec9aaea65450075015f5b33efd0
illashka <93211852+illashka@users.noreply.github.com>
2024-01-29 17:28:34 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -4,6 +4,6 @@ let sum = 0;
 
 for (i of votes){
     if (i.textContent.slice(18) != 'ба'){
-        sum = sum + parseInt(i.textContent.slice(18))/2}}
+        sum = sum + parseInt(i.textContent.slice(23))/2}}
 
 console.log('Количество подписей у Надеждина: ' + sum);
